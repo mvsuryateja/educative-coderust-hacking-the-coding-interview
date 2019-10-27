5c54d1db274fe75f92af0b7ce20d9d4faa14861c
Suryateja M V <mvsuryateja@gmail.com>
2019-10-27 17:43:07 +0530

    Move zeros to left

---
+++ src/main/java/com/interviews/educative/arrays/G_MoveZerosToLeft.java
@@ -0,0 +1,39 @@
+package com.interviews.educative.arrays;
+
+import java.util.Arrays;
+
+public class G_MoveZerosToLeft {
+
+	private static int[] INPUT = { 0, 1, 10, -1, 11, 5, 10, -7, 10, 25, -35 };
+
+	public static void main(String[] args) {
+		moveZerosToLeft(INPUT);
+
+		System.out.println(Arrays.toString(INPUT));
+	}
+
+	private static void moveZerosToLeft(int[] input) {
+		if (input == null || input.length == 0) {
+			return;
+		}
+
+		int writer = input.length - 1;
+
+		for (int reader = input.length - 1; reader >= 0; reader--) {
+			if (input[reader] == 0) {
+				continue;
+			}
+
+			if (reader == writer) {
+				writer--;
+				continue;
+			}
+
+			input[writer] = input[reader];
+			input[reader] = 0;
+
+			writer--;
+		}
+	}
+
+}
